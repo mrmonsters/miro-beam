2c23d693187be02f5122cf14cd262c9026ca68ff
Chin Tiong <alvin.tanct@gmail.com>
2024-03-06 10:11:27 +0000

    add reddit link handler

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
       </head>
       <body>
         <Script src="https://miro.com/app/static/sdk/v2/miro.js" strategy="beforeInteractive" />
-        <div id="root">
-        {children}
-        </div>
+        <div id="root">{children}</div>
       </body>
     </html>
   );
--- src/hooks/useHandleItemsCreate.tsx
+++ src/hooks/useHandleItemsCreate.tsx
@@ -2,10 +2,14 @@ import { ItemsCreateEvent } from '@mirohq/websdk-types';
 import { handleLinkItemsCreate } from '../utils/miro';
 import { useEffect } from 'react';
 
+const handler = (event: ItemsCreateEvent) => {
+  handleLinkItemsCreate(event);
+};
+
 export const useHandleItemsCreate = () => {
   useEffect(() => {
-    window.miro.board.ui.on('items:create', (event: ItemsCreateEvent) => {
-      handleLinkItemsCreate(event);
-    });
+    window.miro.board.ui.on('items:create', handler);
+
+    return () => miro.board.ui.off('items:create', handler);
   }, []);
 };
